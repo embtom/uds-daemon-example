3e7ec64346b14b167eb6aab594b51a38bb8f9626
Thomas Willetal <t.willetal@gmail.com>
2025-10-12 11:57:01 +0000

    Basic service

--- sockact-example/sockact-a/sockact-a.cpp
+++ sockact-example/sockact-a/sockact-a.cpp
@@ -1,6 +1,62 @@
+#include <signalhandler.h>
+#include <spdlog/sinks/stdout_color_sinks.h>
+#include <spdlog/sinks/systemd_sink.h>
 #include <spdlog/spdlog.h>
+#include <sd_notify.h>
+#include <systemd/sd-daemon.h> // für sd_booted()
 
 int main(int argc, const char** argv)
 {
-    spdlog::info("Hello");
+    (void)argc;
+    (void)argv;
+    bool theEnd{false};
+
+    std::shared_ptr<spdlog::logger> logger;
+
+    if (sd_booted() > 0) {
+        auto systemd_sink = std::make_shared<spdlog::sinks::systemd_sink_mt>("sockact-a");
+        logger = std::make_shared<spdlog::logger>("", systemd_sink);
+    } else {
+        auto console_sink = std::make_shared<spdlog::sinks::stdout_color_sink_mt>();
+        logger = std::make_shared<spdlog::logger>("", console_sink);
+    }
+
+    spdlog::set_default_logger(logger);
+    spdlog::set_level(spdlog::level::debug);
+
+    utils::CSignalHandler signalHandler({SIGTERM, SIGINT, SIGHUP});
+
+    try {
+        signalHandler.EnableSigsegvHandler();
+    } catch (const std::exception& e) {
+        spdlog::error("Error occurred {}", e.what());
+        theEnd = true;
+    }
+
+    spdlog::info("App start");
+    systemd_notify::ready();
+    while (!theEnd) {
+        const int receivedSignal = signalHandler.Sigwait();
+        switch (receivedSignal) {
+        case SIGTERM: [[fallthrough]];
+        case SIGINT: {
+            spdlog::info("Termination requested");
+            systemd_notify::stopping();
+            theEnd = true;
+            break;
+        }
+        case SIGHUP: {
+            systemd_notify::reloading();
+            spdlog::info("Hook for reloading configuration");
+            systemd_notify::ready();
+            break;
+        }
+        default: {
+            spdlog::warn("Unhandled signal received {}", receivedSignal);
+        }
+        }
+    }
+
+    spdlog::info("Exiting...");
+    return EXIT_SUCCESS;
 }
